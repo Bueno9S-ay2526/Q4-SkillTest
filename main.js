b20ee07b3b966470f11ed7510fbdfb5b93dbeb3e
Alex.com <bueno.zat@obmontessori.edu.ph>
2026-05-05 08:52:10 +0800

    woops

--- main.js
+++ main.js
@@ -1,17 +1,1 @@
-var u, p
-u = "Con2Solee";
-p = "C0nn0r!sH0mo";
 
-function LogIn() {
-    let uinput = document.getElementById("userN").value;
-    let pinput = document.getElementById("passW").value;
-
-    if (u === uinput && p === pinput) {
-        r = "Welcome back, Con2Solee! :D";
-        
-    } else {
-        r = "WRONG!";
-        
-    }
-    document.getElementById("result").innerHTML = r;
-}
